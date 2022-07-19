d1840683be34cb1b92f7e53c047e61e779016a39
Muney <muneyyyy@gmail.com>
2022-07-19 22:48:59 +0800

    Initial Commit
    -add/remove projects/to-do items from respective containers
    -no connection between projects and to-do items

---
+++ index.bundle.js
@@ -0,0 +1,23 @@
+(self["webpackChunktop_todolist"] = self["webpackChunktop_todolist"] || []).push([["index"],{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+const test = document.createElement("p");
+test.textContent = "Does this work?";
+const div = document.querySelector(".content");
+test.classList.add("test-class");
+div.appendChild(test);
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/index.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7O0FBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQSIsInNvdXJjZXMiOlsid2VicGFjazovL3RvcC10b2RvbGlzdC8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zdCB0ZXN0ID0gZG9jdW1lbnQuY3JlYXRlRWxlbWVudChcInBcIik7XG50ZXN0LnRleHRDb250ZW50ID0gXCJEb2VzIHRoaXMgd29yaz9cIjtcbmNvbnN0IGRpdiA9IGRvY3VtZW50LnF1ZXJ5U2VsZWN0b3IoXCIuY29udGVudFwiKTtcbnRlc3QuY2xhc3NMaXN0LmFkZChcInRlc3QtY2xhc3NcIik7XG5kaXYuYXBwZW5kQ2hpbGQodGVzdCk7Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
